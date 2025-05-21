42e8166003c9cb47754312b46c5a311665afe5c8
杉原悠太 <yuta0521sugi@gmail.com>
2025-05-21 13:16:27 +0900

    add libopencm3

--- board_v2_plus4/board.hpp
+++ board_v2_plus4/board.hpp
@@ -1,197 +1,77 @@
-#pragma once
-#include <libopencm3/stm32/adc.h>
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/flash.h>
-#include <libopencm3/stm32/spi.h>
-#include <mculib/fastwiring.hpp>
-#include <mculib/softi2c.hpp>
-#include <mculib/softspi.hpp>
-#include <mculib/si5351.hpp>
-#include <mculib/adf4350.hpp>
-#include <mculib/dma_adc.hpp>
+#ifndef BOARD_HPP
+#define BOARD_HPP
 
-#include <array>
-#include <stdint.h>
-#include <libopencm3/stm32/f1/dma.h>
-#include <libopencm3/stm32/adc.h>
+// !!!!! 重要 !!!!!
+// 1. GD32ファミリー定義:
+//    本来はMakefileのコンパイルフラグで定義すべきマクロです。
+//    GD32F303CCはHigh-densityデバイスの可能性が高いため、GD32F30X_HD を試します。
+//    正しい定義は、お使いのlibopencm3のドキュメントやGD32のシリーズ分類を確認してください。
+#ifndef GD32F30X_HD // Makefileで定義されていない場合のフォールバック
+#define GD32F30X_HD
+#endif
+// 他の可能性として: GD32F303ZE (具体的な型番), GD32F30X_CL など。
 
-#include "../rfsw.hpp"
-#include "../common.hpp"
-#include "../xpt2046.hpp"
+// 2. Makefileの修正勧告:
+//    Makefile内の EXTRA_CFLAGS から -DMCULIB_DEVICE_STM32F103, -DSTM32F103, -DSTM32F1
+//    といったSTM32関連の定義を【必ず削除】し、代わりに上記のような適切なGD32ファミリーの
+//    定義 (例: -DGD32F30X_HD) を追加してください。これが最も重要な修正です。
 
-#define BOARD_NAME "NanoVNA V2Plus4"
-#define BOARD_REVISION (4)
-#define BOARD_REVISION_MAGIC 0xdeadbabf
-#define USB_POINTS_MAX 65536
-// Plus4 not use ecal mode
-#define BOARD_DISABLE_ECAL
+// libopencm3のインクルードパスのルートが -Ilibopencm3/include となっていることを前提とする
+// ヘッダファイルが libopencm3/gd32/ の直下にあると仮定して修正
+#include <libopencm3/gd32/rcc.h>
+#include <libopencm3/gd32/gpio.h>
+#include <libopencm3/gd32/spi.h>
+// #include <libopencm3/gd32/f3/rcc.h>  // 修正前
+// #include <libopencm3/gd32/f3/gpio.h> // 修正前
+// #include <libopencm3/gd32/f3/spi.h>  // 修正前
 
-using namespace mculib;
-using namespace std;
+#include "common.hpp"
+#include "spi_slave.hpp" // SPIスレーブ処理のヘッダをインクルード
 
-// This not used in Plus4 code, need only for bootloader
-#define BOARD_MEASUREMENT_NPERIODS_NORMAL		14
-#define BOARD_MEASUREMENT_NPERIODS_CALIBRATING	30
-#define BOARD_MEASUREMENT_ECAL_INTERVAL			 5
-#define BOARD_MEASUREMENT_NWAIT_SWITCH			 1
-#define BOARD_MEASUREMENT_MIN_CALIBRATION_AVG	10
-#define BOARD_MEASUREMENT_MAX_CALIBRATION_AVG	255
-#define BOARD_MEASUREMENT_FIRST_POINT_WAIT     128
+// BOARD_REVISION は spi_config.h またはビルドフラグで定義されることを想定
 
-namespace board {
+// クロック周波数
+#define HSE_VALUE    8000000U
+#define SYSCLK_FREQ_120MHz 120000000U
 
-	// ##### pin assignments #####
+#ifndef F_CPU
+#define F_CPU SYSCLK_FREQ_120MHz
+#endif
 
-	static constexpr Pad led = PA6;
-	static constexpr Pad led2 = PA7;
-	static constexpr Pad USB0_DP = PA12;
-	static constexpr Pad USB0_DM = PA11;
+// SPIピン定義
+#define SPI1_PORT GPIOA
+#define SPI1_CLK_PIN GPIO5
+#define SPI1_MISO_PIN GPIO6
+#define SPI1_MOSI_PIN GPIO7
+#define SPI1_NSS_PIN GPIO4
 
-	static constexpr array<Pad, 2> RFSW_ECAL = {PC13, PC14};
-	static constexpr array<Pad, 2> RFSW_BBGAIN = {PB15, PB14};
-	static constexpr Pad RFSW_TXSYNTH = PA5;
-	static constexpr Pad RFSW_RXSYNTH = PA10;
-	static constexpr Pad RFSW_REFL = PA8;
-	static constexpr Pad RFSW_RECV = PA9;
+// その他ボード固有定義
+#define BUTTON_PORT GPIOA
+#define BUTTON_PIN GPIO0
 
-	
-	static constexpr Pad lcd_clk = PB3;
-	static constexpr Pad lcd_mosi = PB5;
-	static constexpr Pad lcd_miso = PB4;
-	static constexpr Pad ili9341_cs = PA15;
-	static constexpr Pad ili9341_dc = PB6;
-	static constexpr Pad xpt2046_cs = PB7;
-	static constexpr Pad xpt2046_irq = PB8;
+#define LED_GREEN_PORT GPIOC
+#define LED_GREEN_PIN GPIO13
 
-	static constexpr Pad LEVER_LEFT = PB11;
-	static constexpr Pad LEVER_CENTER = PB12;
-	static constexpr Pad LEVER_RIGHT = PB13;
-	static constexpr bool LEVER_POLARITY = false; // pin level when lever/button is pressed
+// 関数プロトタイプ
+void boardInit();
+void led_set(bool on);
+bool button_pressed();
+void delay_ms(uint32_t ms);
 
-	// ##### board parameters #####
-	
-	// estimated HSE frequency in Hz, set by boardInit()
-	extern uint32_t hseEstimateHz;
+#if BOARD_REVISION >= 2
+#define si5351_i2c_dev I2C0
+#define SI5351_I2C_ADDRESS (0x60 << 1)
+void si5351_init();
+#endif
 
-	// All boards use a 24Mhz TCXO. It gives best phase noise with the ADF4350
-	static constexpr uint32_t xtalFreqHz = 24000000;
-	static constexpr freqHz_t DEFAULT_FREQ = 2600000000;
+#if BOARD_REVISION >= 4
+#define SYS_setHWSweep 100
+#define SYS_setHWLO1 101
+#define SYS_setHWLO2 102
+#define SYS_setHWMode 103
+#define SYS_setHWGain 104
+#define SYS_HWreadADC 105
+#define SYS_HWsetAttn 106
+#endif
 
-	// ADC parameters, set by boardInit()
-	extern uint32_t adc_ratecfg;
-	extern uint32_t adc_srate; // Hz
-	extern uint32_t adc_period_cycles, adc_clk;
-	constexpr int adc_rxChannel = 0;
-
-	// the end of flash memory. User data is stored before this point.
-	constexpr uint32_t USERFLASH_END = 0x08000000 + 256*1024;
-
-
-	// ##### board peripherals #####
-
-	extern DMADriver dma;
-	extern DMAChannel dmaChannelADC;
-	extern DMAADC dmaADC;
-
-	// synthesizers
-
-	struct i2cDelay_t {
-		void operator()() {
-			_delay_8t(5);
-		}
-	};
-	struct spiDelay_t {
-		void operator()() {
-			_delay_8t(2);
-		}
-	};
-
-	extern SoftI2C<i2cDelay_t> si5351_i2c;
-	extern Si5351::Si5351Driver si5351;
-
-	extern SoftSPI<spiDelay_t> adf4350_tx_spi;
-	extern SoftSPI<spiDelay_t> adf4350_rx_spi;
-
-	struct adf4350_sendWord_t {
-		SoftSPI<spiDelay_t>& spi;
-		void operator()(uint32_t word) {
-			spi.beginTransfer();
-			spi.doTransfer_send(word, 32);
-			spi.endTransfer();
-		}
-	};
-	extern ADF4350::ADF4350Driver<adf4350_sendWord_t> adf4350_tx;
-	extern ADF4350::ADF4350Driver<adf4350_sendWord_t> adf4350_rx;
-
-	constexpr int si5351_rxPLL = 0, si5351_txPLL = 1;
-	constexpr int si5351_rxPort = 0, si5351_txPort = 2, si5351_passthruPort = -1;
-
-	// lcd display
-
-	extern XPT2046 xpt2046;
-
-	// rf switch positions
-
-	static constexpr auto RFSW_ECAL_SHORT = RFSWState::RF4;
-	static constexpr auto RFSW_ECAL_OPEN = RFSWState::RF3;
-	static constexpr auto RFSW_ECAL_LOAD = RFSWState::RF2;
-	static constexpr auto RFSW_ECAL_NORMAL = RFSWState::RF1;
-
-	static constexpr int RFSW_TXSYNTH_LF = 0;
-	static constexpr int RFSW_TXSYNTH_HF = 1;
-
-	static constexpr int RFSW_RXSYNTH_LF = 1;
-	static constexpr int RFSW_RXSYNTH_HF = 0;
-
-	static constexpr int RFSW_REFL_ON = 1;
-	static constexpr int RFSW_REFL_OFF = 0;
-
-	static constexpr int RFSW_RECV_REFL = 0;
-	static constexpr int RFSW_RECV_PORT2 = 1;
-
-	static constexpr int RFSW_BBGAIN_MAX = 3;
-
-	// gain is an integer from 0 to 3, 0 being lowest gain
-	static inline RFSWState RFSW_BBGAIN_GAIN(int gain) {
-		switch(gain) {
-			case 0: return RFSWState::RF1;
-			case 1: return RFSWState::RF2;
-			case 2: return RFSWState::RF3;
-			case 3: return RFSWState::RF4;
-			default: return RFSWState::RF4;
-		}
-		return RFSWState::RF4;
-	}
-
-	// call this function at the beginning of main()
-	void boardInit();
-
-	// returns an estimate of the HSE frequency in Hz.
-	// called by boardInit() to set hseEstimateHz.
-	uint32_t detectHSEFreq();
-
-	// blink the status led
-	void ledPulse();
-	
-	int calculateSynthWaitAF(freqHz_t freqHz);
-	int calculateSynthWaitSI(int retval);
-
-	// sets up hardware spi for ili9341 and touch.
-	// spi peripheral only manages clk, sdi, and sdo.
-	void lcd_spi_init();
-
-	// three speed presets for ili9341 write/read and touch controller
-	void lcd_spi_write();
-	void lcd_spi_read();
-	void lcd_spi_slow();
-
-	// bits must be 16 or 8
-	uint32_t lcd_spi_transfer(uint32_t sdi, int bits);
-
-	void lcd_spi_transfer_bulk(uint8_t* buf, int bytes);
-
-	void lcd_spi_read_bulk(uint8_t* buf, int bytes);
-
-	// wait for all bulk transfers to complete
-	void lcd_spi_waitDMA();
-}
+#endif // BOARD_HPP
